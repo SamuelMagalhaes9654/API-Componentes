c8354b5edf14fc16edfd2820ea7d9b196fff8360
Samuel Magalhães <samuel9654@gmail.com>
2023-05-16 16:23:09 -0300

    liberando o CORS

--- src/main/java/APIComponentes/Projeto/config/SecurityConfig.java
+++ src/main/java/APIComponentes/Projeto/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import APIComponentes.Projeto.service.UsuarioService;
 
@@ -23,7 +24,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests(//(authz) -> authz
+        http
+        .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+        .and().csrf().disable().authorizeHttpRequests(//(authz) -> authz
             authorizeConfig -> {
                 authorizeConfig.requestMatchers("/componentes").hasRole("ADMIN");
                 //authorizeConfig.requestMatchers("/usuarios").permitAll();
@@ -32,6 +35,7 @@ public class SecurityConfig {
                 
         ).formLogin().and().httpBasic();
         return http.build();
+        //.formLogin().and()
     }
 
     // @Bean
--- src/main/java/APIComponentes/Projeto/service/UsuarioService.java
+++ src/main/java/APIComponentes/Projeto/service/UsuarioService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.server.ResponseStatusException;
 
 import APIComponentes.Projeto.domain.Usuario;
